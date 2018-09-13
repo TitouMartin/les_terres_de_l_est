6e8ca714f7c32bbed5efd5029b303d7988d2fe1a
Titouan Martin <titouan.martin@free.fr>
2018-09-13 15:49:31 +0200

    started on the album view

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,10 +1,11 @@
-class AlbumController < ApplicationController
+class AlbumsController < ApplicationController
+  before_action :sort_albums, only: [:index]
   before_action :set_album, only: [:show]
   before_action :album_tracks, only: [:show]
   before_action :album_artist, only: [:show]
 
   def index
-    @albums = albums_sorted
+    @albums = @albums_sorted
   end
 
   def show
@@ -17,7 +18,7 @@ class AlbumController < ApplicationController
   private
 
   def sort_albums
-    albums_sorted = Album.all.sort_by!{ |a| a.name.downcase }
+    @albums_sorted = Album.all.sort_by{ |a| a.name.downcase }
    end
 
   def set_album
@@ -29,6 +30,9 @@ class AlbumController < ApplicationController
   end
 
   def album_artist
-    album_artist = album_tracks.artist
+    album_artist = []
+    Track.where(album: @album).each do |track|
+      album_artist << track.artist
+    end
   end
 end
