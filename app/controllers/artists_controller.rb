b64d01546e2f09f62f3966f499d7a192c3333792
Titouan Martin <titouan.martin@free.fr>
2018-09-13 12:35:29 +0200

    artist controller ok

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,36 @@
 class ArtistsController < ApplicationController
+  before_action :set_atist, only: [:show]
+  before_action :artist_tracks, only: [:show]
+  before_action :artist_albums, only: [:show]
+  def index
+    @artists = artists_sorted
+  end
+
+  def show
+    render :index unless @artist
+    @artist_tracks = artist_tracks
+    @artist_albums = artist_albums
+  end
+
+
+
+  private
+
+
+  def sort_artists
+    artists_sorted = Artist.all.sort_by!{ |a| a.name.downcase }
+  end
+
+  def set_artist
+    @artist = Artist.find(params[:id])
+  end
+
+  def artist_tracks
+    artist_tracks = Track.where(artist: @artist)
+  end
+
+  def artist_albums
+    artist_albums = artist_tracks.album
+  end
+
 end
