3919ab0418dbd2fcca5c826d710080f1ec43b592
Titouan Martin <titouan.martin@free.fr>
2018-09-13 15:24:43 +0200

    created a first small seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+20.times do
+  Artist.create(
+    name: Faker::Artist.name,
+    localisation: Faker::TwinPeaks.location,
+    description: Faker::GameOfThrones.quote
+  )
+  print '.'
+end
+puts 'artists created'
+
+20.times do
+  Album.create(
+    name: Faker::Pokemon.name,
+    cover: 'https://images.unsplash.com/photo-1496293455970-f8581aae0e3b?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=fb87cdb308815f5e6decf7d1a3a4e7ca&auto=format&fit=crop&w=1001&q=80',
+    format_price_vinyl: 12,
+    format_price_digital: 0,
+    stock: rand(10...100),
+    description: Faker::MichaelScott.quote
+  )
+  print '.'
+end
+puts 'albums created'
+
+100.times do
+  Track.create(
+    name: Faker::LordOfTheRings.character,
+    description: Faker::HarryPotter.quote,
+    album: Album.all.sample,
+    artist: Artist.all.sample
+  )
+
+  print '.'
+end
+puts 'tracks created'
